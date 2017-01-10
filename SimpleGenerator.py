ccfa3791d549c5372301471a5d2b8a43abda9a43
Mark Yalenti <myalenti@mongodb.com>
2017-01-10 13:31:17 -0500

    Change to reflect requested document and faker was moved outside of functions - best performance

--- SimpleGenerator.py
+++ SimpleGenerator.py
@@ -2,7 +2,6 @@ import random
 import threading
 import time
 import logging
-import pymongo
 import sys
 import timeit
 import multiprocessing
@@ -12,6 +11,8 @@ import names
 import random
 import os
 import numpy
+import datetime
+import pymongo
 from faker import Factory
 from collections import OrderedDict
 
@@ -27,14 +28,18 @@ password=""
 database="axa"
 collection="profiles"
 logLevel=logging.INFO
-procCount=2
-totalDocuments=1000
+procCount=3
+totalDocuments=1000000
 tstart_time = time.time()
 executionTimes=[]
+seqBase = 100000000
+
+faker = Factory.create()
+
 
 #preGenerating random list for use in document with a random integer
 randomList=[]
-for i in xrange(22):
+for i in xrange(5):
     randomList.append(random.random())
 
 
@@ -56,7 +61,7 @@ def connector():
         sys.exit()
     return
 
-def generateDocument():
+def generateDocument(seqId):
     #randSequence = random.sample(xrange(9999),21)
     randSeed = random.randint(0, 100)
     randConfuser = randomList[1]
@@ -72,62 +77,52 @@ def generateDocument():
     lastname = "ln"
     email = "fn_ln" + "@mongodb.com"
     record = OrderedDict()
-    record['name']  = OrderedDict()
-    record['name']['firstName'] = firstname
-    record['name']['lastName'] = lastname
-    record['contact'] = OrderedDict()
-    record['contact']['email'] = email
-    record['nfld1'] = randAnchor / randomList[0]
-    record['nfld2'] = randAnchor / randomList[1]
-    record['nfld3'] = randAnchor / randomList[2]
-    record['nfld4'] = randAnchor / randomList[3]
-    record['nfld5'] = randAnchor / randomList[4]
-    record['nfld6'] = randAnchor / randomList[5]
-    record['nfld7'] = randAnchor / randomList[6]
-    record['nfld8'] = randAnchor / randomList[7]
-    record['nfld9'] = randAnchor / randomList[8]
-    record['nfld10'] = randAnchor / randomList[9]
-    record['nfld11'] = randAnchor / randomList[10]
-    record['nfld12'] = randAnchor / randomList[11]
-    record['nfld13'] = randAnchor / randomList[12]
-    record['nfld14'] = randAnchor / randomList[13]
-    record['nfld15'] = randAnchor / randomList[14]
-    record['nfld16'] = randAnchor / randomList[15]
-    record['nfld17'] = randAnchor / randomList[16]
-    record['nfld18'] = randAnchor / randomList[17]
-    record['nfld19'] = randAnchor / randomList[18]
-    record['nfld20'] = randAnchor / randomList[19]
-    #record['nfld21'] = randSequence[21]
-    #record['nfld22'] = randSequence[22]
-    #record['nfld23'] = randSequence[23]
-    #record['nfld24'] = randSequence[24]
-    #record['nfld25'] = randSequence[25]
-    #record['nfld26'] = randSequence[26]
-    #record['nfld27'] = randSequence[27]
-    #record['nfld28'] = randSequence[28]
-    #record['nfld29'] = randSequence[29]
-    #record['nfld30'] = randSequence[1]
-    #record['strfld1'] = fakeText[0:20]
-    #record['strfld2'] = fakeText[21:40]
-    #record['strfld3'] = fakeText[41:60]
-    #record['strfld4'] = fakeText[61:80]
-    #record['strfld5'] = fakeText[81:100]
-    #record['strfld6'] = fakeText[101:120]
-    #record['strfld7'] = fakeText[121:140]
-    #record['strfld8'] = fakeText[141:160]
-    #record['strfld9'] = fakeText[161:180]
-    #record['strfld10'] = fakeText[181:200]
-
+    #Four Text Fields
+    record['SeqId'] = seqId
+    record['Text1'] = faker.word()
+    record['Text2'] = faker.word()
+    record['Text3'] = faker.word()
+    
+    #Nine Date/Time Fields
+    record['Date1'] = datetime.datetime.utcnow()
+    record['Date2'] = datetime.datetime.utcnow()
+    record['Date3'] = datetime.datetime.utcnow()
+    record['Date4'] = datetime.datetime.utcnow()
+    record['Date5'] = datetime.datetime.utcnow()
+    record['Date6'] = datetime.datetime.utcnow()
+    record['Date7'] = datetime.datetime.utcnow()
+    record['Date8'] = datetime.datetime.utcnow()
+    record['Date9'] = datetime.datetime.utcnow()
+    #Four Float Fields
+    record["Float1"] = randAnchor / randomList[0]
+    record["Float2"] = randAnchor / randomList[1]
+    record["Float3"] = randAnchor / randomList[2]
+    record["Float4"] = randAnchor / randomList[3]
+    #Four Integer Fields
+    record['Integer1'] = random.randint(0,100)
+    record['Integer2'] = random.randint(0,200)
+    record['Integer3'] = random.randint(0,300)
+    record['Integer4'] = random.randint(0,400)
+    #Four Bool fields
+    bolList = []
+    bolList.append(True)
+    bolList.append(False)
+    record['Bool1'] = random.choice( bolList)
+    record['Bool2'] = random.choice( bolList)
+    record['Bool3'] = random.choice( bolList)
+    record['Bool4'] = random.choice( bolList)
+    
     #print record
     logging.debug( "Document : %s" % (str(record)))
     return record
 
-def worker(iterations):
+def worker(iterations, seqBase):
     connection = connector()
     db = connection[database]
     col = db[collection]
 
     itCounter = 0
+    seqNumb = seqBase
 
     while ( itCounter < iterations):
         logging.debug("Entering Iterator")
@@ -135,8 +130,9 @@ def worker(iterations):
         request = []
         while (counter < bulkSize):
             logging.debug("Entering Bulk Counter")
-            request.append(InsertOne(generateDocument()))
+            request.append(InsertOne(generateDocument(seqNumb)))
             counter += 1
+            seqNumb += 1
         try:
             logging.debug("%s" % str(request))
             start_time = time.time()
@@ -144,7 +140,7 @@ def worker(iterations):
             end_time = time.time()
             execTime = end_time - start_time
             logging.debug("%s" % str(bulk_result.bulk_api_result))
-            logging.info("Executed in : %s" % str(execTime))
+            logging.debug("Executed in : %s" % str(execTime))
             executionTimes.append(execTime)
             #print len(executionTimes)
 
@@ -167,11 +163,16 @@ jobs = []
 setSize = totalDocuments / procCount
 #Iterations is the number of times the worker function has to execute to deliver the total number of docs in a sets
 bulkCount = setSize/bulkSize
-logging.info("Set Size is %d, and bulk count is %d given bulk size of %d" % (setSize, bulkCount, bulkSize))
+procSeqBase = seqBase  
+
+logging.info("Set Size is %d, and iteration count is %d given bulk size of %d" % (setSize, bulkCount, bulkSize))
 for i in range(procCount):
-    p = multiprocessing.Process(target=worker, args=(bulkCount,))
+    p = multiprocessing.Process(target=worker, args=(bulkCount,procSeqBase,))
     p.start()
     jobs.append(p)
+    procSeqBase = seqBase + ( (i+1) * setSize)
+    logging.info("Process id %d with base of %d"  % ( i , procSeqBase ))
+  
 
 for i in jobs:
     i.join()
